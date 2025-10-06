250ddb7c6992d086c15e60d6a3e5f16d6b20afc6
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2025-10-06 12:33:00 +0300

    added addition, division and subtraction.

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -1,18 +1,17 @@
 public class PrinterApp {
     public static void main(String[] args) throws Exception {
-        String app = "This app is made by : ";
-        String creator = "Subhanjan";      // made the code some what clearer by moving the declarations and 
+        String app = "This app is made by : ";     // made the code some what clearer by moving the declarations
         int number1 = 5;
         int number2 = 2;
-        int multiplication = number1 * number2;
-        int addition = number1 + number2;
+        int addition = number1 + number2;        // added addition, divison and subtration
         int subtraction = number1 - number2;
+        int multiplication = number1 * number2;
         double division = (double) number1 / number2;   
         System.out.println("Printer App");
         System.out.println(app+creator);
-        System.out.println("The value of number1 is " + number1);
+        System.out.println("The value of number1 is " + number1);                
         System.out.println(number1 + " * " + number2 + " = " + multiplication);  // adjusted spaces as required from the assignment 
-        System.out.println(number1 + " + " + number2 + " = " + addition);     // added addition, divison and subtration
+        System.out.println(number1 + " + " + number2 + " = " + addition);     
         System.out.println(number1 + " / " + number2 + " = " + division); 
         System.out.println(number1 + " - " + number2 + " = " + subtraction);
     }
