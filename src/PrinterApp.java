a8489641cf59739f796e8217b700d9c2bb93e5f7
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2025-10-06 12:02:49 +0300

    Initial commit

---
+++ src/PrinterApp.java
@@ -0,0 +1,13 @@
+public class PrinterApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Printer App");
+        String app = "This app is made by : ";
+        String creator = "Subhanjan";
+        System.out.println(app+creator);
+        int number1 = 5;
+        int number2 = 2;
+        System.out.println("The value of number1 is " + number1);
+        int multiplication = number1 * number2;
+        System.out.println(number1 + "*" + number2 + "=" + multiplication);
+    }
+}
