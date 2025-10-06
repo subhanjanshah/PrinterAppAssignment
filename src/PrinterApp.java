c77bac714330b0fdaf1e38507e0c1c787611feaf
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2025-10-06 12:26:41 +0300

    adjusted some spaces as mention in the assignment question

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -1,13 +1,19 @@
 public class PrinterApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Printer App");
         String app = "This app is made by : ";
-        String creator = "Subhanjan";
-        System.out.println(app+creator);
+        String creator = "Subhanjan";      // made the code some what clearer by moving the declarations and 
         int number1 = 5;
         int number2 = 2;
-        System.out.println("The value of number1 is " + number1);
         int multiplication = number1 * number2;
-        System.out.println(number1 + "*" + number2 + "=" + multiplication);
+        int addition = number1 + number2;
+        int subtraction = number1 - number2;
+        double division = (double) number1 / number2;   
+        System.out.println("Printer App");
+        System.out.println(app+creator);
+        System.out.println("The value of number1 is " + number1);
+        System.out.println(number1 + " * " + number2 + " = " + multiplication);  // adjusted spaces as required from the assignment 
+        System.out.println(number1 + " + " + number2 + " = " + addition);     // added addition, divison and subtration
+        System.out.println(number1 + " / " + number2 + " = " + division); 
+        System.out.println(number1 + " - " + number2 + " = " + subtraction);
     }
 }
